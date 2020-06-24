8cde5f097ed6c694185a3d12a940acfc9d05ab9e
sungho <dloere3@naver.com>
2020-06-24 11:53:41 +0900

    서명 이더리움.. 미완성

--- ecdsa.js
+++ ecdsa.js
@@ -1,7 +1,8 @@
 import key from "./key.js";
 import secp256k1 from "secp256k1";
 import crypto from "crypto";
-
+import createkeccakHash from "keccak";
+import web3 from "web3"
 
 
 //secp256k로 서명을 하려면 메세지를 고정된 32바이트로 만들어야함. ==> 해쉬함수로 만든다. (SHA256, Keccak256) 256비트는 32바이트
@@ -18,15 +19,31 @@ function recover(message, signature){
     return Buffer.from(secp256k1.ecdsaRecover(signature.signature, signature.recid, hash))
 }
 
+//이더리움 사인에서는 message를 바로 해쉬하지 않고 여러가지를 추가한 후에 해쉬함. 
+function ethSign(message, privateKey){
+    const prefix = "\x19Ethereum Signed Message:\n" + message.length;
+    const buffer = Buffer.from(prefix + message);
+    const hash = createkeccakHash("keccak256").update(buffer).digest();
+    return secp256k1.ecdsaSign(hash, privateKey);//개인키로 암호화 해서 서명
+}
+
+// 이더리움은 공개키가 아니라 주소를 복원함.
+function ethRecover(message, signature) {
+    const prefix = "\x19Ethereum Signed Message:\n" + message.length;
+    const buffer = Buffer.from(prefix + message);
+    const hash = createkeccakHash("keccak256").update(buffer).digest();
+    const publicKey = Buffer.from(secp256k1.ecdsaRecover(signature.signature ,signature.recid, hash, false));
+    // 공개키를 알아냄.
+    const address = key.createAddress(publicKey); 
+    return key.toChecksumAddress(address); //이더리움은 주소를 복원
+}
+
+
 const privateKey = key.createPrivatekey();
-const publicKey = key.createPublicKey2(privateKey);
-console.log("public key : ", publicKey. toString("hex"));
-const message = "hello blockchain"
-console.log(message)
+const address = key.privateKeyToAddress(privateKey);
+console.log("sender address", address);
 
+const signature =  ethSign("hello Blockchain", privateKey);
+const recoveredAddress = ethRecover("hello Blockchain", signature)
+console.log("recovered address : ", recoveredAddress);
 
-// const {signature2 , recoveryId } = sign("hello blockchain", privateKey);
-const signature = sign(message, privateKey);
-const recoveredkey = recover(message, signature);
-console.log(signature)
-console.log("recoveredKey : ", recoveredkey)
